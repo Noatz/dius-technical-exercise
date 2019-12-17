665c9940b2ce2ba6d8d7da120159270f8ef299a5
Ethan Catzel <ethancatzel@gmail.com>
2019-12-17 15:31:49 +1100

    Added frames and interactability

--- src/BowlingGame.py
+++ src/BowlingGame.py
@@ -3,9 +3,59 @@ from typing import List
 class BowlingGame:
     def __init__(self):
         self.rolls: List[int] = []
+        self.frames: int = 0
+        self.cur_frame_rolls: int = 0
+        self.extra_bowls: int = 0
 
-    def roll(self, *pins: int) -> None:
-        self.rolls += pins
+    def get_frame_count(self) -> int:
+        return self.frames
+
+    def roll(self, pins: int) -> None:
+        # error check: pin number
+        if pins > 10 or pins < 0:
+            print ('ERROR: unsuccessful roll, pins must be in the range of 0 and 10')
+            return
+        # error check: frame can't knock more than 10 pins down
+        if self.cur_frame_rolls == 1 and self.rolls[-1] != 10 and 10-self.rolls[-1] - pins < 0:
+            print ('ERROR: max pins down in one frame can only be 10')
+            return
+
+        # check if final frame
+        if self.frames == 10:
+            if self.extra_bowls <= 0:
+                print ('Sorry, but the game is already over.')
+            elif self.extra_bowls > 0:
+                self.rolls.append(pins)
+                self.extra_bowls -= 1
+            return
+
+        # max 2 rolls per frame
+        if self.cur_frame_rolls == 2:
+            self.cur_frame_rolls = 0
+
+        self.rolls.append(pins)
+
+        self.cur_frame_rolls += 1
+        # finished another frame, given the following conditions
+        if self.cur_frame_rolls == 2 or pins == 10:
+            self.frames += 1
+
+            # check if finished last frame
+            if self.frames == 10:
+                # last bowl was a strike
+                if self.rolls[-1] == 10:
+                    print ('Congrats, you get another 2 bowls!!')
+                    self.extra_bowls = 2
+                    return
+                # last bowl was a spare
+                if sum(self.rolls[-2:]) == 10:
+                    print ('Congrats, you get another bowl!')
+                    self.extra_bowls = 1
+                    return
+
+    def _roll_many(self, *pins_list: int) -> None:
+        for pins in pins_list:
+            self.roll(pins)
 
     def score(self) -> int:
         cur_score: int = 0
@@ -35,3 +85,22 @@ class BowlingGame:
             roll_num += 2
 
         return cur_score
+
+
+if __name__ == '__main__':
+    bg = BowlingGame()
+
+    while True:
+        user = input('')
+        method, *other = user.lower().split()
+
+        if method == 'roll':
+            numbers: List[int] = list(map(int, other))
+            bg._roll_many(*numbers)
+        elif method == 'score':
+            print (f'> {bg.score()}')
+        elif method in ['done', 'end', 'exit']:
+            print ('Thanks for playing :)')
+            exit(0)
+
+    print (f'Your final score is: {bg.score()}')
--- src/BowlingGameTests.py
+++ src/BowlingGameTests.py
@@ -5,41 +5,76 @@ class Test(unittest.TestCase):
 
     def test_normal_game(self):
         bg = BowlingGame()
-        bg.roll(3, 6, 1, 7, 4, 4)
+        bg._roll_many(3, 6, 1, 7, 4, 4)
         self.assertEqual(bg.score(), 25)
-    
+        self.assertEqual(bg.get_frame_count(), 3)
+
     def test_spare_game(self):
         # start with a spare
         bg = BowlingGame()
-        bg.roll(4, 6, 5, 0)
+        bg._roll_many(4, 6, 5, 0)
         self.assertEqual(bg.score(), 20)
+        self.assertEqual(bg.get_frame_count(), 2)
 
         # mid-game with a spare
         bg = BowlingGame()
-        bg.roll(2, 3, 5, 5, 2, 7)
+        bg._roll_many(2, 3, 5, 5, 2, 7)
         self.assertEqual(bg.score(), 26)
+        self.assertEqual(bg.get_frame_count(), 3)
 
         # end with a spare
         bg = BowlingGame()
-        bg.roll(3, 5)
-        bg.roll(9, 1)
+        bg._roll_many(3, 5, 9, 1)
         self.assertEqual(bg.score(), 18)
-    
+        self.assertEqual(bg.get_frame_count(), 2)
+
     def test_strike_game(self):
         # start with a strike
         bg = BowlingGame()
-        bg.roll(10, 5, 4)
+        bg._roll_many(10, 5, 4)
         self.assertEqual(bg.score(), 28)
+        self.assertEqual(bg.get_frame_count(), 2)
 
         # mid-game with a strike
         bg = BowlingGame()
-        bg.roll(7, 1, 10, 3, 3)
+        bg._roll_many(7, 1, 10, 3, 3)
         self.assertEqual(bg.score(), 30)
+        self.assertEqual(bg.get_frame_count(), 3)
 
         # end-game with a strike
         bg = BowlingGame()
-        bg.roll(4, 1, 10)
+        bg._roll_many(4, 1, 10)
         self.assertEqual(bg.score(), 15)
+        self.assertEqual(bg.get_frame_count(), 2)
+
+    def test_frames(self):
+        # normal: 10 frames
+        bg = BowlingGame()
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        self.assertEqual(bg.get_frame_count(), 10)
+        self.assertEqual(bg.score(), 40)
+
+        # normal: 11 frames
+        bg = BowlingGame()
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        self.assertEqual(bg.get_frame_count(), 10)
+        self.assertEqual(bg.score(), 40)
+
+        # spare: 11 frames
+        bg = BowlingGame()
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 8, 2)
+        self.assertEqual(bg.get_frame_count(), 10)
+        self.assertEqual(bg.score(), 50)
+
+        # strike: 11 frames
+        bg = BowlingGame()
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 2, 2)
+        bg._roll_many(2, 2, 2, 2, 2, 2, 2, 2, 10, 4, 5)
+        self.assertEqual(bg.get_frame_count(), 10)
+        self.assertEqual(bg.score(), 64)
 
 if __name__ == '__main__':
     unittest.main()
